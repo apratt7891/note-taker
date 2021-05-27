247ba20ea33f850e19fdc2cfe6d2bf667519aac7
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-05-26 20:59:41 -0600

    api route

---
+++ routes/apiRoutes.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const app = require('express');
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public.notes.html"));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public.index.html"));
+});
+
+
+
+
+
+
+
+
+
+
+
+module.exports = 
