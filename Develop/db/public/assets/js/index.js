737e45db709085cdf3c7026ecb0e6bbfd25fd657
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-05-29 09:43:07 -0600

    add heroku

--- Develop/db/public/assets/js/index.js
+++ Develop/db/public/assets/js/index.js
@@ -1,15 +1,16 @@
-let noteTitle = $('.note-title');
-let noteText = $('.note-textarea');
-let saveNoteBtn = $('.save-note');
-let newNoteBtn = $('.new-note');
-let noteList = $('.list-container .list-group');
+let noteTitle;
+let noteText;
+let saveNoteBtn;
+let newNoteBtn;
+let noteList;
 
-
-// activeNote is used to keep track of the note in the textarea
-let activeNote = {};
-
-
-
+if (window.location.pathname === '/notes') {
+  noteTitle = document.querySelector('.note-title');
+  noteText = document.querySelector('.note-textarea');
+  saveNoteBtn = document.querySelector('.save-note');
+  newNoteBtn = document.querySelector('.new-note');
+  noteList = document.querySelectorAll('.list-container .list-group');
+}
 
 // Show an element
 const show = (elem) => {
@@ -21,33 +22,33 @@ const hide = (elem) => {
   elem.style.display = 'none';
 };
 
+// activeNote is used to keep track of the note in the textarea
+let activeNote = {};
 
-
-const getNotes = () => {
-  return $.ajax({
-    url: '/api/notes',
-    method: 'GET'
+const getNotes = () =>
+  fetch('/api/notes', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
   });
-};
-  
 
-const saveNote = (note) => {
-  return $.ajax({
-    url: '/api/notes',
-    data: note,
-    method: 'POST'
+const saveNote = (note) =>
+  fetch('/api/notes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(note),
   });
-};
-  
-  
 
-const deleteNote = (id) => {
-  return $.ajax({
-    url: `/api/notes/${id}`,
-    method: 'DELETE'
-  })
-}
-  
+const deleteNote = (id) =>
+  fetch(`/api/notes/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
 const renderActiveNote = () => {
   hide(saveNoteBtn);
