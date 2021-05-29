d0665c6c5b7a16b850ad2a7ec9d023745e920dfb
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-05-29 09:45:05 -0600

    change port for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 
 const express = require('express');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static(__dirname));
